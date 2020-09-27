d5369b710304c065723894f93abb700c8c5d00fb
Your Name <you@example.com>
2020-09-27 12:37:05 +0200

    services

---
+++ case_management/case_management/doctype/intended_use_of_funds/intended_use_of_funds.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Akram Mutaher and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Intended Use of Funds', {
+	// refresh: function(frm) {
+
+	// }
+});
